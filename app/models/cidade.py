0ff7f2244945de835cfbc0557ba63215f061c15e
djairb <djair.batista@outlook.com>
2019-06-11 21:55:13 -0300

    init de Cidade

--- app/models/cidade.py
+++ app/models/cidade.py
@@ -1,10 +1,16 @@
 class Cidade(db.Model):
-    
+
     __tablename__ = "cidade"
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     nome = db.Column(db.String(45), nullable=False)
     latitudeELongitude = db.Column(db.String(45), nullable=False)
+
+    def __init__(self, id, nome, latitudeELongitude):
+        self.id = id
+        self.nome = nome
+        self.latitudeELongitude = latitudeELongitude
+    
     
 
 
