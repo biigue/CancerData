03aa772f38fbf78e8e280f7fdcc053d208c28ee1
Bianca Cardoso <biancapcardoso@outlook.com>
2019-06-11 23:55:39 -0300

    sem testar

---
+++ app/models/ano.py
@@ -0,0 +1,17 @@
+class LocationObj:
+    def __init__(self):
+        self.Ano =  None
+        self.idAno = None
+
+
+    def get_Ano(self):
+        return self.Ano
+
+    def set_Ano(self, Ano):
+        self.Ano = Ano
+
+    def get_idAno(self):
+        return self.idAno
+
+    def set_idAno(self, idAno):
+        self.idAno = idAno
