99a2352134a37a2abdc2eb56e314879a8f492fa5
Unknown <djair.batista@outlook.com>
2019-06-11 21:51:14 -0300

    Cidade Class
    (incompleto)

--- app/models/cidade.py
+++ app/models/cidade.py
@@ -1,2 +1,10 @@
-class Cidade():
+class Cidade(db.Model):
     
+    __tablename__ = "cidade"
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    nome = db.Column(db.String(45), nullable=False)
+    latitudeELongitude = db.Column(db.String(45), nullable=False)
+    
+
+
