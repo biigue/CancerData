6897a63c791d125f4636b313b7c417541fe35821
Unknown <djair.batista@outlook.com>
2019-06-20 12:05:17 -0300

    Get's e Set's em mortalidade

--- app/models/mortalidade.py
+++ app/models/mortalidade.py
@@ -6,3 +6,40 @@ class MortalidadeObj():
         self.raca = None
         self.FaixaEtaria = None
         self.Ano_idAno = None
+
+    def get_idMortalidade(self):
+        return self.idMortalidade
+
+    def set_idMortalidade(self, idMortalidade) :
+        self.idMortalidade = idMortalidade
+
+    def get_numeroDeCasos(self):
+        return self.numeroDeCasos
+
+    def set_numeroDeCasos(self, numeroDeCasos) :
+        self.numeroDeCasos = numeroDeCasos
+
+    def get_tipoDeCancer(self):
+        return self.tipoDeCancer
+
+    def set_tipoDeCancer(self, tipoDeCancer) :
+        self.tipoDeCancer = tipoDeCancer
+
+    def get_raca(self):
+        return self.raca
+
+    def set_raca(self, raca) :
+        self.raca = raca
+
+    def get_FaixaEtaria(self):
+        return self.FaixaEtaria
+
+    def set_FaixaEtaria(self, FaixaEtaria) :
+        self.FaixaEtaria = FaixaEtaria
+
+    def get_Ano_idAno(self):
+        return self.Ano_idAno
+
+    def set_Ano_idAno(self, Ano_idAno) :
+        self.Ano_idAno = Ano_idAno
+
